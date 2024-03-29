7a72b761540e2056d0d60a8c8026f5c55a9b2bdf
Anzhela-Ostrovska1 <108702919+Anzhela-Ostrovska1@users.noreply.github.com>
2024-03-29 21:22:12 +0000

    Deploying to gh-pages from @ Anzhela-Ostrovska1/goit-group2-react-nodejs@aaf52a899a4afb3fe8ae5982b526f5838f4bf50d 🚀

---
+++ assets/Home-57e81d70.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-b5dc0321.js";function r(){return e.jsx("div",{children:"Home"})}export{r as default};
--- assets/SignUp-3b552157.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-3adbfedb.js";function r(){return i.jsx("div",{children:"SignUp"})}export{r as default};
