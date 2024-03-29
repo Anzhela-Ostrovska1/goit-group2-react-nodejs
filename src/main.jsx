4223edd68e879d14b979cdd3b7ac676a07afeaa2
Anzhela Ostrovska <anzhelk451964@gmail.com>
2024-03-29 14:07:18 -0700

    changed import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Global, ThemeProvider } from '@emotion/react';
 import { GlobalStyles, theme } from './styles';
-import App from 'components/App';
+import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
