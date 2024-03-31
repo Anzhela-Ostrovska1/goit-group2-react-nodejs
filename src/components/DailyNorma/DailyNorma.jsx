2a46c21c166ae449d8a2d286c842b095b181c2d6
TauruS111 <yuriybereza85@gmail.com>
2024-03-31 10:23:29 +0200

    start

---
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const DailyNorma = () => {
+  return (
+    <div>
+      <div>
+        <h2>My daily norma:</h2>
+        <div>
+          <p>1.5 L</p>
+          <button type="button">Edit</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DailyNorma;
