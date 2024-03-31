4012a73a27c7c88ffc44caaab07d40ff32e41724
OksanaKorsun <oksana2081989@gmail.com>
2024-04-01 00:30:06 +0300

    styled-main-page

--- src/components/Main/WaterConsumptionTracker/WaterConsumptionTracker.styled.jsx
+++ src/components/Main/WaterConsumptionTracker/WaterConsumptionTracker.styled.jsx
@@ -18,16 +18,12 @@ export const Heading = styled.h2`
   font-size: 28px;
   font-weight: 700;
   line-height: 32px;
-  /* text-align: left;
-  display: inline-block;  */
   vertical-align: top;
   margin-bottom: 16px;
   @media only screen and (min-width: 768px) {
     font-size: 36px;
     line-height: 42px;
   }
-  /* @media only screen and (min-width: 1440px) {
-    } */
 `;
 export const Title = styled.h3`
   font-size: 24px;
