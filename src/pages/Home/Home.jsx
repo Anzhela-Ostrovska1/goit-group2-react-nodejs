55f052fb42d808b0f11ed9da31a9157bf24b097d
TauruS111 <yuriybereza85@gmail.com>
2024-03-31 18:57:01 +0200

    fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,3 @@
-import DailyNorma from '../../components/DailyNorma/DailyNorma';
-import WaterRatioPanel from '../../components/WaterRatioPanel/WaterRatioPanel';
-
 export default function Home() {
-  return (
-    <div>
-      <DailyNorma />
-      <WaterRatioPanel />
-    </div>
-  );
+  return <div>Home</div>;
 }
