e5f0acfb76d89f4ff8d830ce71ced7de27450bf2
олена городюк <135028180+OlenaUser1982@users.noreply.github.com>
2024-04-02 13:13:35 +0300

    2

---
+++ src/components/TimePicker/TimePicker.jsx
@@ -0,0 +1,47 @@
+import { Select, Wrapper } from './TimePicker.styled';
+export default function TimePicker({ onChange, value }) {
+  const hours = Array.from(new Array(24), (val, index) => index);
+  const minutes = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+
+  const valueDate = new Date(value);
+
+  let valueHour = valueDate.getHours();
+  let valueMinute = valueDate.getMinutes();
+
+  const closestMinute = minutes.reduce(
+    (prev, curr) =>
+      curr >= valueMinute
+        ? prev === null ||
+          Math.abs(curr - valueMinute) < Math.abs(prev - valueMinute)
+          ? curr
+          : prev
+        : prev,
+    null,
+  );
+  if (closestMinute === null) {
+    valueHour = (valueHour + 1) % 24;
+    valueMinute = 0;
+  } else {
+    valueMinute = closestMinute;
+  }
+
+  return (
+    <Wrapper id="selectTimeWrapper">
+      <Select onChange={e => onChange(e, 'hour')} value={valueHour}>
+        {hours.map(hour => (
+          <option key={hour} value={hour}>
+            {hour.toString().padStart(2, '0')}
+          </option>
+        ))}
+      </Select>
+      :
+      <Select onChange={e => onChange(e, 'minute')} value={valueMinute}>
+        {minutes.map(minute => (
+          <option key={minute} value={minute}>
+            {minute.toString().padStart(2, '0')}
+          </option>
+        ))}
+      </Select>
+    </Wrapper>
+  );
+}
