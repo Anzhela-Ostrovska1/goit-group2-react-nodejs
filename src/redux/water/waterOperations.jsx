f274b8955b81d727bf467811b76329ddd56df97e
Ihor Berezhnyi <iberezhnyi@gmail.com>
2024-04-05 16:46:17 +0300

    feat(api, waterOperations, waterSelectors, waterSlice): add water thunks, selectors, state

--- src/redux/water/waterOperations.jsx
+++ src/redux/water/waterOperations.jsx
@@ -1,6 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-import { addWater, editWater } from '../Api/api';
+import {
+  addWater,
+  editWater,
+  deleteWater,
+  fetchTodayWater,
+  fetchCurrentWater,
+  fetchMonthWater,
+} from '../Api/api';
 export const addWaterThunk = createAsyncThunk(
   'water/add',
   async (note, { rejectWithValue }) => {
@@ -12,11 +19,60 @@ export const addWaterThunk = createAsyncThunk(
     }
   },
 );
+
 export const editWaterThunk = createAsyncThunk(
   'water/edit',
   async (note, { rejectWithValue }) => {
     try {
       const data = await editWater(note);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
+
+export const deleteWaterThunk = createAsyncThunk(
+  'water/delete',
+  async (waterId, { rejectWithValue }) => {
+    try {
+      const data = await deleteWater(waterId);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
+
+export const fetchCurrentWaterThunk = createAsyncThunk(
+  'water/fetchCurrentWater',
+  async (waterId, { rejectWithValue }) => {
+    try {
+      const data = await fetchCurrentWater(waterId);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
+
+export const fetchTodayWaterThunk = createAsyncThunk(
+  'water/fetchTodayWater',
+  async (_, { rejectWithValue }) => {
+    try {
+      const data = await fetchTodayWater();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
+
+export const fetchMonthWaterThunk = createAsyncThunk(
+  'water/fetchMonthWater',
+  async (body, { rejectWithValue }) => {
+    try {
+      const data = await fetchMonthWater(body);
       return data;
     } catch (error) {
       return rejectWithValue(error.message);
