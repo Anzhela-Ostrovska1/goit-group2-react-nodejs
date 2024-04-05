7afd64c78822c1175735130eb3d344bc25575aac
TauruS111 <yuriybereza85@gmail.com>
2024-04-05 16:10:41 +0200

    add/startoday

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { FaPlus } from 'react-icons/fa6';
 
 const TodayWaterList = () => {
-  return <div>TodayWaterList</div>;
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  // };
+  // const openModal = () => {
+  //   setIsModalOpen(true);
+  // };
+  return (
+    <div>
+      <div>
+        <h2>Today</h2>
+      </div>
+      <div>{/* <use href={}></use> */}</div>
+      <div>
+        <button>
+          <FaPlus /> Add Water
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TodayWaterList;
