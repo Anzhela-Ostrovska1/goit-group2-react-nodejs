daf307ee869dd21806ba6e5f06f392f8995af8e1
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2024-04-05 19:22:37 +0200

    dailyNormalModal

--- src/components/DailyNormaModal/DailyNormaModal.jsx
+++ src/components/DailyNormaModal/DailyNormaModal.jsx
@@ -2,49 +2,50 @@ import React from 'react'
 import { DailyNormaModalContainer } from './DailyNormaModal.styled.js'
 
 const DailyNormaModal = () => {
-          return (
-                    <div>
-                              <DailyNormaModalContainer>
-                                        <div className='headerDaily'>
-                                                  <h2>My daily norma</h2>
-                                                  <button type="button" data-modal-close>
-                                                            <svg className="modal-icon" width="24" height="24">
-                                                              <use href="/src/assets/images/sprite/icon-user"></use>
-                                                            </svg>
-                                                          </button>
-                                        </div>
-                                        <div className='formula'><p className='text'>For girl:<span>V=(M*0,03) + (T*0,4)</span></p>
-                                                  <p>For man:<span>V=(M*0,04) + (T*0,6)</span></p>
-                                        </div>
-                                        
-                                                  <p className='value'>* V is the volume of the water norm in liters per day, M is your body weight, T is the time of active sports, or another type of activity commensurate in terms of loads (in the absence of these, you must set 0)</p>
-                                        <form>
-                                        <h3>Calculate your rate:</h3>
-                                                  <div className='checkBox'>
-                                                            {/* <fieldset> */}
-                                                                      
-                                                                      <label><input type='radio'name='gender' value='w' />For woman</label>
-                                                                      
-                                                                      <label><input type='radio' name='gender' value='m' />For man</label>
+return (
+    <div>
+      <DailyNormaModalContainer>
+        <div className='headerDaily'>
+          <h2>My daily norma</h2>
+          <button type="button" data-modal-close>
+            <svg className="icon-Vector" width="24" height="24" >
+              <use href="../../assets/images/sprite/sprite.svg#icon-Vector"></use>
+            </svg>
+          </button>
+        </div>
+        <div className='formulaContainer'>
+          <div className='formula'>
+            <div className='formulaText'>
+              <p>For girl:</p><span className='formulaSpan'>V=(M*0,03) + (T*0,4)</span>
+            </div>
+            <div className='formulaText'>
+              <p>For man:</p><span className='formulaSpan'>V=(M*0,04) + (T*0,6)</span>
+            </div>
+          </div>
+          <p className='formulaDescription'><span className='formulaStar'>*</span> V is the volume of the water norm in liters per day, M is your body weight, T is the time of active sports, or another type of activity commensurate in terms of loads (in the absence of these, you must set 0)</p>
+        </div>
 
-                                                                      
+        <form>
+          <h3>Calculate your rate:</h3>
+          <div className='checkBox'>
+            <label><input type='radio' name='gender' value='woman' checked /> For woman</label>
+            <label><input type='radio' name='gender' value='man' /> For man</label>
+          </div>
 
-                                                            {/* </fieldset> */}
-                                                  </div>
-                                        <label className='text'>Your weight in kilograms:</label>
-                                        <input />
-                                        <label className='text'>The time of active participation in sports or other activities with a high physical. load in hours:</label>
-                                        <input />
-                                        <div className='layout'><p className='text'>The required amount of water in liters per day:</p><span className='amount'>1.8 L</span></div>
-                                        <h3>Write down how much water you will drink:</h3>
-                                                  <input className='inputForm'/>
+          <label className='inputDescription'>Your weight in kilograms:</label>
+          <input className='inputForm' placeholder="0" />
+          <label className='inputDescription'>The time of active participation in sports or other activities with a high physical. load in hours:</label>
+        <input className='inputForm' placeholder="0" />
+          <div className='requiredAmount'><p className='amountDescription'>The required amount of water in liters per day:</p><span className='amount'>1.8 L</span></div>
+          <h3>Write down how much water you will drink:</h3>
+          <input className='inputForm' placeholder="0"/>
                                                  
-                                                  <input className="button" type="submit" value="Save" />
-                                                   </form>
-                              </DailyNormaModalContainer>
+          <input className="saveButton" type="submit" value="Save" />
+        </form>
+      </DailyNormaModalContainer>
                       
-                    </div>
-          )
-}
+    </div>
+  )
+};
 
 export default DailyNormaModal
--- src/components/DailyNormaModal/DailyNormaModal.styled.js
+++ src/components/DailyNormaModal/DailyNormaModal.styled.js
@@ -6,9 +6,24 @@ export const DailyNormaModalContainer = styled.div`
   top: 40px;
   left: 20px;
   padding: 24px 12px 24px 12px;
+  display: flex;
+  flex-direction: column;
   gap: 24px;
-  border-radius: 10px 0px 0px 0px;
-  opacity: 0px;
+  border-radius: 10px;
+  margin: auto;
+  @media screen and (min-width: 768px) {
+    width: 704px;
+    height: 696px;
+    left: 32px;
+    padding: 32px 24px 32px 24px;
+  }
+
+  @media screen and (min-width: 1140px) {
+    width: 592px;
+    height: 712px;
+    top: 44px;
+    left: 424px;
+  }
 
   h2 {
     font-size: 26px;
@@ -18,96 +33,121 @@ export const DailyNormaModalContainer = styled.div`
   }
   .headerDaily {
     display: flex;
-    width: 256px;
+    justify-content: space-between;
     height: 32px;
-    gap: 40px;
-    opacity: 0px;
   }
 
   .formula {
-    display: inline-block;
-    margin-top: 16px;
-    width: 236px;
+    display: flex;
+    flex-direction: column;
     height: 64px;
     gap: 16px;
-    opacity: 0px;
+    @media screen and (min-width: 768px) {
+      flex-direction: row;
+      gap: 24px;
+    }
   }
 
-  .text {
+  .formulaText {
     font-size: 16px;
     font-weight: 400;
     line-height: 20px;
     text-align: left;
+    display: flex;
+    gap: 4px;
   }
-  span {
+  .formulaSpan {
     font-size: 18px;
     font-weight: 400;
     line-height: 24px;
     text-align: left;
     color: #407bff;
   }
+  .formulaStar {
+    color: #407bff;
+  }
 
-  .value {
+  .formulaDescription {
     font-size: 12px;
     font-weight: 400;
     line-height: 16px;
     text-align: left;
-
-    width: 256px;
-    height: 100px;
     padding: 10px;
-    /* gap: 10px; */
-    border-radius: 10px 0px 0px 0px;
-    border: 1px 0px 0px 0px;
-    opacity: 0px;
+    border-radius: 10px;
+    border: solid 1px #d7e3ff;
   }
-
+  .formulaContainer {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+  }
   h3 {
     font-size: 18px;
     font-weight: 500;
     line-height: 20px;
     text-align: left;
-
-    padding-top: 20px;
-    padding-bottom: 20px;
   }
 
   .checkBox {
-    width: 206px;
     height: 20px;
     gap: 24px;
     opacity: 0px;
     display: flex;
-    margin-bottom: 10px;
+  }
+  .checkBox input {
+    border-color: #407bff;
   }
 
-  .layout {
+  form {
     display: flex;
-    width: 253px;
+    flex-direction: column;
+    gap: 16px;
+  }
+  .inputDescription {
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+    text-align: left;
+  }
+  .requiredAmount {
+    display: flex;
+    align-items: center;
     height: 40px;
     gap: 6px;
-    opacity: 0px;
-    margin-top: 15px;
   }
   .amount {
     font-size: 18px;
     font-weight: 700;
     line-height: 24px;
     text-align: left;
+    color: #407bff;
+  }
+  .amountDescription {
+    width: 190px;
   }
   .inputForm {
-    width: 256px;
     height: 44px;
-    margin-bottom: 20px;
+    border: solid 1px #d7e3ff;
+    border-radius: 6px;
+    padding: 12px 10px;
   }
-  .button {
+  .saveButton {
     color: #ffffff;
     background: #407bff;
-    width: 256px;
     height: 36px;
     padding: 8px 30px 8px 30px;
     gap: 10px;
-    /* border-radius: 10px 0px 0px 0px; */
-    opacity: 0px;
+    border-radius: 10px;
+    border: 0;
+    box-shadow: 0px 4px 8px 0px #407bff57;
+    @media screen and (min-width: 768px) {
+      width: 160px;
+      align-self: flex-end;
+    }
+  }
+
+  input::placeholder {
+    color: #407bff;
+    size: 16px;
   }
 `;
