ef262685150367de31ee6e72ff1b6aa827366682
TauruS111 <yuriybereza85@gmail.com>
2024-04-05 19:11:18 +0200

    add/today

--- src/components/TodayWaterList/TodayWaterList.jsx
+++ src/components/TodayWaterList/TodayWaterList.jsx
@@ -1,25 +1,101 @@
 import React from 'react';
 import { FaPlus } from 'react-icons/fa6';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectNotes } from '../../redux/water/waterSelectors';
+import glass from '../../assets/images/background/home/svg/glass.svg';
+import edit from '../../assets/images/background/home/svg/edit.svg';
+import delite from '../../assets/images/background/home/svg/delete.svg';
+import AddWaterModal from '../AddWaterModal/AddWaterModal';
+import EditWaterModal from '../EditWaterModal/EditWaterModal';
 
+import {
+  AddButton,
+  Amount,
+  Time,
+  Title,
+  WaterList,
+  WaterListItem,
+  WaterPortion,
+} from './TodayWaterList.styled';
+import { useState } from 'react';
+
+const testArray = [
+  { id: '1', amount: '250ml', time: '14:00 PM' }, // delete
+  { id: '2', amount: '250ml', time: '14:00 PM' },
+  { id: '3', amount: '250ml', time: '14:00 PM' },
+  { id: '4', amount: '250ml', time: '14:00 PM' },
+  { id: '5', amount: '250ml', time: '14:00 PM' },
+  { id: '6', amount: '250ml', time: '14:00 PM' },
+];
 const TodayWaterList = () => {
-  // const [isModalOpen, setIsModalOpen] = useState(false);
-  // const closeModal = () => {
-  //   setIsModalOpen(false);
-  // };
-  // const openModal = () => {
-  //   setIsModalOpen(true);
-  // };
+  const [waterNotes, setWaterNotes] = useState(testArray); // delete
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  // const dispatch = useDispatch(); open.
+  const waterList = useSelector(selectNotes);
+  console.log(waterList);
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setEditModalOpen(false);
+  };
+  const openEditModal = () => {
+    setEditModalOpen(true);
+  };
+
+  const deleteWaterNote = id => {
+    setWaterNotes(prevNotes => prevNotes.filter(note => note.id !== id)); // delete
+    // dispatch(deleteWaterThunk(id)); open.
+  };
+
   return (
     <div>
       <div>
-        <h2>Today</h2>
+        <Title>Today</Title>
+        <WaterList>
+          {waterNotes.length > 0 && // waterList change waterNotes
+            waterNotes.map(({ id, amount, time }) => {
+              return (
+                <>
+                  {' '}
+                  <WaterListItem key={id}>
+                    <WaterPortion>
+                      <img src={glass} alt="Icon glass" />
+                      <Amount>{amount}</Amount>
+                      <Time>{time}</Time>
+                    </WaterPortion>
+                    <div>
+                      <button type="button" onClick={openEditModal}>
+                        <img src={edit} alt="Icon glass" />
+                      </button>
+                      <button type="button" onClick={() => deleteWaterNote(id)}>
+                        <img src={delite} alt="Icon glass" />
+                      </button>
+                    </div>
+                  </WaterListItem>
+                  {editModalOpen && (
+                    <EditWaterModal
+                      isOpen={editModalOpen}
+                      onClose={closeEditModal}
+                    />
+                  )}
+                </>
+              );
+            })}
+        </WaterList>
       </div>
-      <div>{/* <use href={}></use> */}</div>
-      <div>
-        <button>
-          <FaPlus /> Add Water
-        </button>
-      </div>
+
+      <AddButton type="button" onClick={openModal}>
+        <FaPlus /> Add Water
+      </AddButton>
+      {isModalOpen && (
+        <AddWaterModal isOpen={isModalOpen} onClose={closeModal} />
+      )}
     </div>
   );
 };
--- src/components/TodayWaterList/TodayWaterList.styled.js
+++ src/components/TodayWaterList/TodayWaterList.styled.js
@@ -1,1 +1,73 @@
 import styled from '@emotion/styled';
+
+export const WaterList = styled.ul`
+  overflow-y: scroll;
+  height: 180px;
+  display: flex;
+  flex-direction: column;
+  /* align-items: center; */
+  gap: 12px;
+  list-style: none;
+  @media screen and (min-width: 768px) {
+    height: 176px;
+  }
+`;
+
+export const WaterListItem = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  justify-content: space-between;
+`;
+
+export const WaterPortion = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Amount = styled.p`
+  color: #407bff;
+  font-size: 18px;
+  line-height: 24px;
+  margin-left: 12px;
+`;
+export const Time = styled.p`
+  margin-left: 12px;
+  line-height: 24px;
+  font-size: 12px;
+  color: #2f2f2f;
+
+  @media screen and (min-width: 768px) {
+    margin-left: 16px;
+  }
+`;
+
+export const Title = styled.h2`
+  margin-bottom: 16px;
+  color: #2f2f2f;
+  font-weight: 500px;
+  font-size: 24px;
+  line-height: 30px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 26px;
+    line-height: 32px;
+  }
+`;
+
+export const AddButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 12px;
+  color: #407bff;
+  font-weight: 500px;
+  font-size: 16px;
+  line-height: 20px;
+  gap: 8px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+    line-height: 24px;
+  }
+`;
