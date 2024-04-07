d7e4f0f1624cf6abe5677aa2e918fa8a111acccf
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2024-04-07 20:59:26 +0200

    DailyModalandLogout

--- src/components/DailyNormaModal/DailyNormaModal.jsx
+++ src/components/DailyNormaModal/DailyNormaModal.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from 'react'
-import { DailyNormaModalContainer } from './DailyNormaModal.styled.js'
+import { DailyNormaModalContainer, Wrapper } from './DailyNormaModal.styled.js'
 import { updateWaterRateThunk } from '../../redux/Auth/AuthOperations.jsx';
 import { useDispatch } from 'react-redux';
+import Notiflix from 'notiflix';
 
-const DailyNormaModal = () => {
+const DailyNormaModal = ({onClose}) => {
   const [weight, setWeight] = useState(0);
   const [activity, setActivity] = useState(0);
   const [volume, setVolume] = useState(0);
   const [isChecked, setChecked] = useState(true)
-  const [waterRate, setWaterRate] = useState(0)
-  const [isOpen, setIsOpen] = useState(true)
+  const [waterRate, setWaterRate] = useState(0) 
   const dispatch = useDispatch()
 
   const handleChecked = () => {
@@ -34,22 +34,38 @@ const DailyNormaModal = () => {
   }
 
   const handleSubmit = (evt) => {
-    evt.preventDefault();
-    dispatch(updateWaterRateThunk(waterRate))
-    setIsOpen(!isOpen)
+    try {
+      evt.preventDefault();
+      if (waterRate < 2) return Notiflix.Notify.failure('The required amount of water in liters per day: 2L');
+      if(waterRate >15) return Notiflix.Notify.failure('The maximum amount of water in liters per day: 15L')
+      dispatch(updateWaterRateThunk(waterRate))
+      onClose()
+
+    } catch (error) {
+      console.error('error', error)
+    }
+   
   }
-  const handleClose = () => {
-    setIsOpen(!isOpen)
-  }
+  
+
+  useEffect(() => {
+    const handleKeyDown = event => {
+    if (event.key === 'Escape') {
+      onClose();
+    }
+  };
+    window.addEventListener('keydown', handleKeyDown);
+  }, [onClose]);
+
 
   return (
-    isOpen && <div>
-      <DailyNormaModalContainer>
+    <Wrapper onClick={onClose}>
+      <DailyNormaModalContainer onClick={e => e.stopPropagation()}>
         <div className='headerDaily'>
           <h2>My daily norma</h2>
-          <button type="button" className='closeButton' onClick={handleClose}>
-            <svg className="icon-Vector" width="24" height="24" >
-              <use href="../../assets/images/sprite/horodiukIcons.svg#icon-close"></use>
+          <button type="button" className='closeButton' onClick={onClose}>
+            <svg width="24" height="24" >
+              <use href="/goit-group2-react-nodejs/src/assets/images/sprite/horodiukIcons.svg#icon-close"></use>
             </svg>
           </button>
         </div>
@@ -83,8 +99,52 @@ const DailyNormaModal = () => {
           <button className='saveButton' type='submit'>Save</button>
         </form>
       </DailyNormaModalContainer>        
-    </div>
+    </Wrapper>
   )
+  
+  // return (
+  //   <BaseModalWindow onClose={onClose} onShow={()=>{}} title="My daily norma">
+  //     <DailyNormaModalContainer>
+  //       {/* <div className='headerDaily'>
+  //         <h2>My daily norma</h2>
+  //         <button type="button" className='closeButton' onClick={handleClose}>
+  //           <svg width="24" height="24" >
+  //             <use href="/goit-group2-react-nodejs/src/assets/images/sprite/horodiukIcons.svg#icon-close"></use>
+  //           </svg>
+  //         </button>
+  //       </div> */}
+  //       <div className='formulaContainer'>
+  //         <div className='formula'>
+  //           <div className='formulaText'>
+  //             <p>For girl:</p><span className='formulaSpan'>V=(M*0,03) + (T*0,4)</span>
+  //           </div>
+  //           <div className='formulaText'>
+  //             <p>For man:</p><span className='formulaSpan'>V=(M*0,04) + (T*0,6)</span>
+  //           </div>
+  //         </div>
+  //         <p className='formulaDescription'><span className='formulaStar'>*</span> V is the volume of the water norm in liters per day, M is your body weight, T is the time of active sports, or another type of activity commensurate in terms of loads (in the absence of these, you must set 0)</p>
+  //       </div>
+
+  //       <form onSubmit={handleSubmit}>
+  //         <h3>Calculate your rate:</h3>
+  //         <div className='checkBox'>
+  //           <label><input type='radio' name='gender' value='woman' checked={isChecked} onChange={handleChecked} /> For woman</label>
+  //           <label><input type='radio' name='gender' value='man' checked={!isChecked} onChange={handleChecked} /> For man</label>
+  //         </div>
+
+  //         <label className='inputDescription'>Your weight in kilograms:</label>
+  //         <input className='inputForm' placeholder="0" onChange={handleWeight} />
+  //         <label className='inputDescription'>The time of active participation in sports or other activities with a high physical. load in hours:</label>
+  //         <input className='inputForm' placeholder="0" onChange={handleActivity} />
+  //         <div className='requiredAmount'><p className='amountDescription'>The required amount of water in liters per day:</p><span className='amount'>{volume} L</span></div>
+  //         <h3>Write down how much water you will drink:</h3>
+  //         <input className='inputForm' placeholder="0" onChange={handleWaterRate} />
+                                                 
+  //         <button className='saveButton' type='submit'>Save</button>
+  //       </form>
+  //     </DailyNormaModalContainer>        
+  //   </BaseModalWindow>
+  // )
 };
 
 export default DailyNormaModal
