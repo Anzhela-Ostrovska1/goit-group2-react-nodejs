2f76447fd26900934a28424b8d4204980c1dfcd7
OksanaKorsun <oksana2081989@gmail.com>
2024-04-09 00:39:36 +0300

    button/modal

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -9,26 +9,25 @@ import {
 } from './UserLogoModal.styled';
 
 import { UserLogoutModal } from '../Home/LogOutModal/LogOutModal';
-import  AddWaterModal from '../AddWaterModal/AddWaterModal';
+import { SettingsModal } from '../SettingsModal/SettingsModal';
 
 export const UserLogoModal = () => {
- 
   const [isLogOutModalOpen, setIsLogOutModalOpen] = useState(false);
-  const [isAddWaterModalOpen, setIsAddWaterModalOpen] = useState(false);
+  const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false);
 
   const handleLogOutButtonClick = () => {
     setIsLogOutModalOpen(true);
   };
 
-  const handleAddWaterButtonClick = () => {
-    setIsAddWaterModalOpen(true);
+  const handleSettingsButtonClick = () => {
+    setIsSettingsModalOpen(true);
   };
 
   return (
     <Popover>
       <PopList>
         <PopItem>
-          <PopBtn type="button" onClick={handleAddWaterButtonClick}>
+          <PopBtn type="button" onClick={handleSettingsButtonClick}>
             <PopIcon>
               <use href={`${sprite}#icon-settings`}></use>
             </PopIcon>
@@ -45,7 +44,7 @@ export const UserLogoModal = () => {
         </PopItem>
       </PopList>
       {isLogOutModalOpen && <UserLogoutModal />}
-      {isAddWaterModalOpen && <AddWaterModal />}
+      {isSettingsModalOpen && <SettingsModal />}
     </Popover>
   );
 };
