f4c18263e8b026f4f6509bd108ccdf679dc2cfd9
TauruS111 <yuriybereza85@gmail.com>
2024-04-08 09:52:56 +0200

    add/big-work

--- src/components/Home/DeleteEntryModal/DeleteEntryModal.jsx
+++ src/components/Home/DeleteEntryModal/DeleteEntryModal.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { BaseModalWindow } from '../../common/BaseModalWindow/BaseModalWindow';
+
+import { useDispatch } from 'react-redux';
 import {
   ModalContainer,
   ButtonContainer,
   ButtonStyle,
   ModalTitle,
 } from './DeleteEntryModal.styled';
+import { deleteWaterThunk } from '../../../redux/water/waterOperations';
 
-export const DeleteEntryModal = ({ onClose, onShow }) => {
+export const DeleteEntryModal = ({ onClose, onShow, id }) => {
+  const dispatch = useDispatch();
+  const onDelete = () => {
+    dispatch(deleteWaterThunk(id));
+    onClose();
+  };
+  console.log(id);
   return (
     <BaseModalWindow onClose={onClose} onShow={onShow} title={'Delete Entry'}>
       <ModalContainer>
         <ModalTitle>Are you sure you want to delete the entry?</ModalTitle>
         <ButtonContainer>
-          <ButtonStyle onClick={onClose}>Delete</ButtonStyle>
+          <ButtonStyle onClick={onDelete}>Delete</ButtonStyle>
           <ButtonStyle onClick={onClose}>Cancel</ButtonStyle>
         </ButtonContainer>
       </ModalContainer>
